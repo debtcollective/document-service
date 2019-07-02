23a82eadb516706ee20260262175341a5bcbc945
Alexis Duran <alexis.duranm@gmail.com>
2019-07-02 22:01:32 +0200

    test(documents): disable test that needs CI chrome support

--- src/documents/__tests__/CreditReportDispute.spec.js
+++ src/documents/__tests__/CreditReportDispute.spec.js
@@ -32,7 +32,7 @@ const pathToPDFfolder = path.join(__dirname, "../../../pdf");
 const DocumentHandler = CreditReportDispute;
 
 // NOTE: CI is not able to run this since it relies on Chromium instance
-describe("generateFiles", () => {
+describe.skip("generateFiles", () => {
   let browser;
 
   beforeAll(() => {
