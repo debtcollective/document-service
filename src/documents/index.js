fc1ddb0c4c54378828df8fc9e75ff447241a17d3
Alexis Duran <alexis.duranm@gmail.com>
2019-07-06 14:05:36 +0200

    feat(documents): make reachable all documents type throught `findBySlug`
    re #11

--- src/documents/index.js
+++ src/documents/index.js
@@ -2,7 +2,14 @@
 
 const documentMap = {
   "credit-report-dispute": require("./CreditReportDispute").default,
+  "falsecert-ability-to-benefit": require("./FalseCertAbilityToBenefit")
+    .default,
+  "falsecert-disqualifying": require("./FalseCertDisqualifying").default,
   "general-dispute": require("./GeneralDispute").default,
+  "private-student-loan": require("./PrivateStudentLoan").default,
+  "tax-offset-review": require("./TaxOffsetReview").default,
+  "unauthorized-sign-form": require("./UnauthorizedSignForm").default,
+  "wage-garnishment": require("./WageGarnishment").default,
 };
 
 export const findBySlug = (documentSlug: $Keys<typeof documentMap>) => {
