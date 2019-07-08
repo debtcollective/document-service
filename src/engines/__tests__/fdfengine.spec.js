2f54200a97db8eab878168ad6035e67014258c41
Alexis Duran <alexis.duranm@gmail.com>
2019-07-08 09:02:48 +0200

    test(fdf): add extra test to only verify the ability to mark as checked
    besides, this code will also tweak the test suit to do not remove the generated files but only
    beforeAll tests to run

--- src/engines/__tests__/fdfengine.spec.js
+++ src/engines/__tests__/fdfengine.spec.js
@@ -14,19 +14,29 @@ describe.skip("process", () => {
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
-  });
 
-  beforeEach(() => {
     fs.readdirSync(pathToPDFfolder).forEach(f =>
       fs.unlinkSync(path.join(pathToPDFfolder, f))
     );
+  });
+
+  it("allows to mark checkboxes", async () => {
+    const data = {
+      baseball: "Yes",
+      basketball: "Yes",
+      hockey: "Yes",
+      nascar: "Off",
+    };
+
+    const result = await FDFEngine.process(data, "../stubs/test.pdf");
+    expect(result).toEqual(true);
   });
 
   it("generate a new PDF file from a given data and PDF source file", async () => {
     const preffixPage1 = "topmostSubform[0].Page1[0]";
     // use the utility function `generateFDFTemplate` if needed to generate the FDF object and know the keys
     const data = {
-      // "topmostSubform[0].Page1[0].CheckBox1[0]": true,
+      "topmostSubform[0].Page1[0].CheckBox1[0]": "Yes",
       [`${preffixPage1}.#area[1].SSN1[0]`]: "023",
       [`${preffixPage1}.#area[1].SSN2[0]`]: "45",
       [`${preffixPage1}.#area[1].SSN3[0]`]: "6789",
